75bd0a52658fafcadebc0797388a8a6866ed7427
1skepta <prospaul999@gmail.com>
2025-06-04 18:14:47 +0000

    working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import { Menu, LucideShoppingCart } from "lucide-react";
 
 function Header() {
   return (
     <div>
-      <nav className="bg-[#191919] text-white">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+      <nav className="bg-[#191919] text-white flex items-center justify-between py-7 px-5">
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link> */}
+
+        <Menu size={20} />
+        <span className="text-xl font-extrabold">audiophile</span>
+        <LucideShoppingCart size={20} />
       </nav>
     </div>
   );
