7160b036cc57e97164b7a3af34a95794a849e246
1skepta <prospaul999@gmail.com>
2025-06-04 23:10:48 +0000

    working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,38 @@
 import { Link } from "react-router-dom";
-import { Menu, LucideShoppingCart } from "lucide-react";
+import { Menu, LucideShoppingCart, X } from "lucide-react";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <nav className="bg-[#191919] text-white flex items-center justify-between py-7 px-5">
         {/* <Link to="/">Home</Link>
         <Link to="/about">About</Link> */}
 
-        <Menu size={20} />
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+        </button>
         <span className="text-2xl font-extrabold">audiophile</span>
-        <LucideShoppingCart size={20} />
+        <LucideShoppingCart className="w-6 h-6" />
       </nav>
+      <div className="flex flex-col ">
+        <div className="flex flex-col items-center">
+          image
+          <span>HEADPHONES</span>
+          <span className="text-[#00000080]">SHOP </span>
+        </div>
+        <div className="flex flex-col items-center">
+          image
+          <span>SPEAKERS</span>
+          <span className="text-[#00000080]">SHOP </span>
+        </div>
+        <div className="flex flex-col items-center">
+          image
+          <span>EARPHONES</span>
+          <span className="text-[#00000080]">SHOP </span>
+        </div>
+      </div>
     </div>
   );
 }
