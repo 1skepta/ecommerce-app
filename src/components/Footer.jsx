9773bcbf656189663682ae53c662bcffd9a8499f
1skepta <prospaul999@gmail.com>
2025-06-04 18:41:17 +0000

    working

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+
+function Footer() {
+  return (
+    <div className="bg-[#191919] text-white">
+      <span>audiophile</span>
+      <ul>
+        <li>HOME</li>
+        <li>HEADPHONES</li>
+        <li>SPEAKERS</li>
+        <li>EARPHONES</li>
+      </ul>
+      <span>
+        Audiophile is an all in one stop to fulfill your audio needs. We're a
+        small team of music lovers and sound specialists who are devoted to
+        helping you getthe most out of personal audio. Come and visit our demo
+        facility - we’re open 7 days a week.
+      </span>
+      <span>Copyright 2025. All rights reserved</span>
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer;
