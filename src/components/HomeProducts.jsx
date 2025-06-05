989936399b1fb176eca44edcb91ae48b79553b5e
1skepta <prospaul999@gmail.com>
2025-06-05 11:26:37 +0000

    working

--- src/components/HomeProducts.jsx
+++ src/components/HomeProducts.jsx
@@ -1,11 +1,11 @@
 import speaker from "../assets/home/mobile/image-speaker-zx9.png";
+import zx7 from "../assets/home/mobile/image-speaker-zx7.jpg";
 
 function HomeProducts() {
   return (
     <div className="flex flex-col">
       <div className="relative bg-[#d87d4a] px-6 w-5/6 rounded-xl mx-auto overflow-hidden h-[600px]">
         <div className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/5 w-[500px] h-[500px] bg-no-repeat bg-center bg-contain z-0 bg-[url('/src/assets/home/desktop/pattern-circles.svg')]"></div>
-
         <div className="relative z-10 flex flex-col items-center justify-center text-white text-center h-full">
           <img src={speaker} alt="speaker on my screen" className="w-3/6" />
           <h1 className="text-3xl font-bold my-4">ZX9 SPEAKER</h1>
@@ -17,6 +17,14 @@ function HomeProducts() {
             SEE PRODUCT
           </div>
         </div>
+      </div>
+
+      <div
+        className="bg-cover bg-center rounded-xl w-5/6 mx-auto h-[350px] my-6 flex flex-col items-start px-6 justify-center"
+        style={{ backgroundImage: `url(${zx7})` }}
+      >
+        <h1 className="text-3xl font-semibold ">ZX7 SPEAKER</h1>
+        <div className="mt-9 border border-black px-4 py-3">SEE PRODUCT</div>
       </div>
     </div>
   );
