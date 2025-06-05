b00dcc36276ba82404949fd77fe2f37ab1d176ac
1skepta <prospaul999@gmail.com>
2025-06-05 09:48:08 +0000

    working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
-import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import xx99mobile from "../assets/home/mobile/image-header.jpg";
 
 function Home() {
   return (
     <div>
       <Header />
       <div className="bg-[#191919]">
-        <div></div>
+        <div
+          className="bg-cover bg-center flex items-center justify-center text-white text-center px-4 h-[500px]"
+          style={{ backgroundImage: `url(${xx99mobile})` }}
+        >
+          <div>
+            <h1 className="text-3xl font-bold mb-4">XX99 Mark II Headphones</h1>
+            <p className="text-sm max-w-md mx-auto">
+              Experience natural, lifelike audio and exceptional build quality.
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
