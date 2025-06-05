a32ae78859d393d2c1232b7de85563479eae0a0d
1skepta <prospaul999@gmail.com>
2025-06-05 10:32:15 +0000

    working

--- src/components/HeadOverlay.jsx
+++ src/components/HeadOverlay.jsx
@@ -1,6 +1,7 @@
 import earphones from "../assets/shared/desktop/image-category-thumbnail-earphones.png";
 import headphones from "../assets/shared/desktop/image-category-thumbnail-headphones.png";
 import speakers from "../assets/shared/desktop/image-category-thumbnail-speakers.png";
+import right from "../assets/shared/desktop/icon-arrow-right.svg";
 
 function HeadOverlay() {
   return (
@@ -13,7 +14,10 @@ function HeadOverlay() {
             className="w-36 h-34 -mt-10"
           />
           <h1 className="font-semibold">EARPHONES</h1>
-          <span className="text-[#d87d4a] mt-2 mb-6">SHOP</span>
+          <div className="flex items-center mt-2 mb-6">
+            <span className="text-[#00000080] mr-2">SHOP</span>
+            <img src={right} alt="none" />
+          </div>{" "}
         </div>
         <div className="flex flex-col items-center bg-[#f1f1f1] mx-auto w-4/5 rounded-xl relative mb-16">
           <img
@@ -22,7 +26,10 @@ function HeadOverlay() {
             className="w-36 h-34 -mt-10"
           />
           <h1 className="font-semibold">HEADPHONES</h1>
-          <span className="text-[#d87d4a] mt-2 mb-6">SHOP</span>
+          <div className="flex items-center mt-2 mb-6">
+            <span className="text-[#00000080] mr-2">SHOP</span>
+            <img src={right} alt="none" />
+          </div>{" "}
         </div>
         <div className="flex flex-col items-center bg-[#f1f1f1] mx-auto w-4/5 rounded-xl relative">
           <img
@@ -31,7 +38,10 @@ function HeadOverlay() {
             className="w-36 h-34 -mt-10"
           />
           <h1 className="font-semibold">SPEAKERS</h1>
-          <span className="text-[#d87d4a] mt-2 mb-6">SHOP</span>
+          <div className="flex items-center mt-2 mb-6">
+            <span className="text-[#00000080] mr-2">SHOP</span>
+            <img src={right} alt="none" />
+          </div>
         </div>
       </div>
     </div>
