c1ae05b0146d01eacc486a2c2a3c38c88e654f56
1skepta <prospaul999@gmail.com>
2025-06-05 14:01:54 +0000

    working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import HeadOverlay from "../components/HeadOverlay";
 import HomeProducts from "../components/HomeProducts";
 import HomeAd from "../components/HomeAd";
+import gear from "../assets/shared/mobile/image-best-gear.jpg";
 
 function Home() {
   return (
@@ -11,7 +12,21 @@ function Home() {
       <HomeAd />
       <HeadOverlay />
       <HomeProducts />
-
+      <div className="w-5/6 mx-auto text-center">
+        <img src={gear} alt="gear" className="rounded-xl" />
+        <h1 className="my-6 ">
+          BRINGING YOU THE <span className="text-[#d87d4a]">BEST</span> AUDIO
+          GEAR
+        </h1>
+        <p className="text-[#00000080]">
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </div>
       <Footer />
     </div>
   );
