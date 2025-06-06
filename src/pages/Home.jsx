e9ede519f2d55dbb71e0072179a9a1d6e40b0803
1skepta <prospaul999@gmail.com>
2025-06-06 02:09:36 +0000

    working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,11 +14,11 @@ function Home() {
       <HomeProducts />
       <div className="w-5/6 mx-auto text-center">
         <img src={gear} alt="gear" className="rounded-xl" />
-        <h1 className="my-6 ">
+        <h1 className="my-6 text-3xl font-semibold ">
           BRINGING YOU THE <span className="text-[#d87d4a]">BEST</span> AUDIO
           GEAR
         </h1>
-        <p className="text-[#00000080]">
+        <p className="text-[#00000080] mb-16">
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
