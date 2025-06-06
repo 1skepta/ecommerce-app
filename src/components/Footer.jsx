44a0fd84e39aa025275bc3a2e3d937e64b8710f5
1skepta <prospaul999@gmail.com>
2025-06-06 04:11:22 +0000

    working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import facebook from "../assets/shared/desktop/icon-facebook.svg";
 import instagram from "../assets/shared/desktop/icon-instagram.svg";
 import twitter from "../assets/shared/desktop/icon-twitter.svg";
 import gear from "../assets/shared/mobile/image-best-gear.jpg";
 
 function Footer() {
+  const navigate = useNavigate();
+
+  const footerLinks = [
+    { name: "HOME", path: "/" },
+    { name: "HEADPHONES", path: "/category/headphones" },
+    { name: "SPEAKERS", path: "/category/speakers" },
+    { name: "EARPHONES", path: "/category/earphones" },
+  ];
+
+  const currentYear = new Date().getFullYear();
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
   return (
     <>
       <div className="w-5/6 mx-auto text-center">
@@ -27,10 +43,15 @@ function Footer() {
           audiophile
         </span>
         <ul className="flex flex-col items-center my-10 leading-12">
-          <li>HOME</li>
-          <li>HEADPHONES</li>
-          <li>SPEAKERS</li>
-          <li>EARPHONES</li>
+          {footerLinks.map((link) => (
+            <li
+              key={link.name}
+              onClick={() => handleNavigate(link.path)}
+              style={{ cursor: "pointer" }}
+            >
+              {link.name}
+            </li>
+          ))}
         </ul>
         <span className="text-[#ffffff80] text-center">
           Audiophile is an all in one stop to fulfill your audio needs. We're a
@@ -39,11 +60,11 @@ function Footer() {
           facility - we’re open 7 days a week.
         </span>
         <span className="my-10 text-[#ffffff80]">
-          Copyright 2025. All rights reserved
+          Copyright {currentYear} SKEPTAG. All rights reserved
         </span>
         <div className="flex">
           <img src={twitter} alt="none" />
-          <img src={instagram} alt="none" />
+          <img src={instagram} alt="none" className="mx-6" />
           <img src={facebook} alt="none" />
         </div>
       </div>
